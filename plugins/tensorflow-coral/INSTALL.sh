28afaac4f24e2c7c123fb7b3b6eed22e7cba64bc
Mitch Ross <mitchross09@gmail.com>
2021-01-26 22:47:19 +0000

    pr comments

--- plugins/tensorflow-coral/INSTALL.sh
+++ plugins/tensorflow-coral/INSTALL.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 DIR=`dirname $0`
+echo "Getting coral object detection models..."
+
+wget "https://cdn.shinobi.video/binaries/tensorflow/coral/models-2021-01-26/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite"
+mv ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite models/
+wget "https://cdn.shinobi.video/binaries/tensorflow/coral/models-2021-01-26/plugins_tensorflow-coral_models_coco_labels.txt"
+mv plugins_tensorflow-coral_models_coco_labels.txt coco_labels.txt 
+mv coco_labels.txt models/
+echo "Models downloaded."
 
 npm install yarn -g --unsafe-perm --force
 npm install --unsafe-perm
